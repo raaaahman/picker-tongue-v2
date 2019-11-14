0247619180f4230c15e66a0b2d2f3f266b07194a
raaaahman <sylvain@devindetails.com>
2019-11-14 19:14:06 +0000

    Step 1: First (failing) test

---
+++ src/decode.php
@@ -0,0 +1,11 @@
+<?php 
+
+function pnt_decode( $message, $dictionnary ) {
+
+    foreach( $dictionnary as $code_name => $meaning ) {
+
+        $message = preg_replace( $code_name, $meaning, $message );
+    }
+
+    return $message;
+}
---
+++ tests/test-decode.php
@@ -0,0 +1,17 @@
+<?php 
+
+class pnt_decode_Test extends \PHPUnit\FrameWork\TestCase {
+
+    public function test_decode_messsage() {
+        $message = 'Le coucou a quitté son nid.';
+
+        $dictionary = array(
+            'Le coucou' => 'La cible',
+            'nid'       => 'domicile',
+        );
+
+        $decoded_message = pnt_decode( $message, $dictionary );
+
+        $this->assertEquals( 'La cible a quitté son nid.', $decoded_message );
+    }
+}
