d02fd5d371f3e7c5326a6183034eb82ef86345c0
raaaahman <sylvain@devindetails.com>
2019-11-24 15:21:48 +0100

    Step 2: Correcting PHP error

--- src/decode.php
+++ src/decode.php
@@ -4,7 +4,7 @@ function pnt_decode( $message, $dictionnary ) {
 
     foreach( $dictionnary as $code_name => $meaning ) {
 
-        $message = preg_replace( $code_name, $meaning, $message );
+        $message = preg_replace( '/' . $code_name . '/', $meaning, $message );
     }
 
     return $message;
