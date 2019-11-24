4caf3cfbb772688a41ce1e24202270636db7b4b5
raaaahman <sylvain@devindetails.com>
2019-11-24 16:43:50 +0100

    Step 8: First integration test (with WordPress)

--- tests/test-decode.php
+++ tests/test-decode.php
@@ -1,6 +1,6 @@
 <?php 
 
-class pnt_decode_Test extends \PHPUnit\FrameWork\TestCase {
+class pnt_decode_Test extends WP_UnitTestCase {
 
     public function test_decode_messsage() {
         $message = 'Le coucou a quitté son nid.';
@@ -21,5 +21,18 @@ class pnt_decode_Test extends \PHPUnit\FrameWork\TestCase {
         $this->expectException( ArgumentCountError::class );
 
         $decoded_message = pnt_decode( $message );
+    }
+
+    public function test_decode_from_post_title() {
+
+        $post_id = $this->factory()->post->create( array(
+            'post_title' => 'Vol au dessus d\'un nid de coucous.'
+        ) );
+
+        $this->go_to( '?p=' . $post_id );
+
+        $decoded_title = the_title();
+
+        $this->expectOutputString( 'Vol au dessus d\'un domicile de cibles.', $decoded_title );
     }
 }
